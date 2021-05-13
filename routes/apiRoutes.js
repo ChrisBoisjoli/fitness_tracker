836c25ef0a3ad10ab48b6ecc0db4ad57d9db517c
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-05-13 13:55:34 -0500

    added api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,4 +12,49 @@ router.get('/api/workouts', (req, res) => {
     });
 });
 
+router.get('/api/workouts/range', (req, res) => {
+    /*Workout.find({})*/
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration'
+                }
+            }
+        }
+    ])
+    .sort({day: -1}).limit(7)
+    .then(data => {
+        console.log(data);
+        res.json(data.reverse());
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.post('/api/workouts', ({body}, res) => {
+    console.log("bodyweight", body)
+    Workout.create(body)
+    .then(data => {
+        console.log(data);
+        res.json(data);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.put('/api/workouts/:id', ({body, params}, res) => {
+    console.log("the body is here", body)
+    Workout.updateOne({ _id : params.id }, {$push: { exercises: body }}, {new: true})
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+
 module.exports = router;
