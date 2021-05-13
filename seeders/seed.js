d9730c02d89b1deeee6dce4ed89f371888350783
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-05-13 13:10:32 -0500

    started schema workout

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
-let db = require("../models");
+let db = require("../models/Workout");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://localhost/fitnesstracker", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
