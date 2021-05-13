c8ad6d0699d19522c58937ca46d3e5a146ce22e0
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-05-13 13:49:25 -0500

    add route get workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
 const Workout = require('../models/Workout');
 
+router.get('/api/workouts', (req, res) => {
+    Workout.find({})
+    .then(data => {
+        console.log(data)
+        res.json(data);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
 module.exports = router;
